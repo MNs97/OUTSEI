fde61c6822b80d469e9e702ecca49b7d4cccd3d5
MN's <mc.at.bw08@gmail.com>
2020-04-21 12:57:49 +0900

    SetProgram
    New

---
+++ app/src/main/java/com/websarva/wings/android/outsei/MainActivity.java
@@ -0,0 +1,20 @@
+package com.websarva.wings.android.outsei;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private class inner{
+　　　　 fun onClick()
+    }
+
+}
